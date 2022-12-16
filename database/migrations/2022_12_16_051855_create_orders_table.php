c3426f17f27bb356b2cccd8eb881e75b85a53406
gitCodetrinityMit <mit.codetrinity@gmail.com>
2022-12-16 11:05:32 +0530

    small changes

--- database/migrations/2022_12_16_051855_create_orders_table.php
+++ database/migrations/2022_12_16_051855_create_orders_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('order_id')->nullable();
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
             $table->string('price');
